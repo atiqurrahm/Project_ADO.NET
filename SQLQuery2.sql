f3144f0957e004fc0cc987a1d7354a486171da9e
S M Atiqur Rahman <106535064+atiqurrahm@users.noreply.github.com>
2022-09-29 15:36:17 +0600

    Add files via upload

---
+++ SQLQuery2.sql
@@ -0,0 +1,18 @@
+﻿create table Shoes
+(
+ShoeId INT PRIMARY KEY,
+Model NVARCHAR(30) NOT NULL,
+FirstIntroducedOn DATE NOT NULL,
+Active BIT NOT NULL DEFAULT 1,
+Picture NVARCHAR(150) NOT NULL
+)
+GO
+create table Stocks
+(
+StockId INT PRIMARY KEY,
+Size NVARCHAR(15) NOT NULL,
+Price MONEY NOT NULL,
+StockQuantity INT NOT NULL,
+ShoeId INT NOT NULL REFERENCES Shoes (ShoeId)
+)
+GO
